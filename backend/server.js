740b3e993339c0827d311cf6c31ddc65ca4e3f22
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-16 17:24:10 +0530

    express router and api routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 require('dotenv').config();
+const workoutRoutes = require('./routes/workouts');
 //express app
 const app = express();
 
+app.use(express.json());
 app.use((req,res,next) => {
     console.log(req.path,req.method);
     next()
 })
 
+app.use('/api/workouts',workoutRoutes);
+
 app.listen(process.env.PORT,() => {
     console.log('listening on port',process.env.PORT);
 });
-
-app.get('/',(req,res) => {
-    res.json({message : 'Welcome to the app'});
-})
