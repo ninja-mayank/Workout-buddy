aea2272a68bb48002b42d01bd5bf1035c43f3e25
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-18 11:52:20 +0530

    controller part2

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,7 +2,9 @@ const express = require('express')
 const{
     createWorkout,
     getWorkout,
-    getSingleWorkout
+    getSingleWorkout,
+    deleteWorkout,
+    updateWorkout
 } = require('../controllers/workoutControllers')
 const router = express.Router()
 
@@ -16,13 +18,9 @@ router.get('/:id', getSingleWorkout)
 router.post('/', createWorkout)
 
 // DELETE a workout
-router.delete('/:id', (req, res) => {
-  res.json({mssg: 'DELETE a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/:id', (req, res) => {
-  res.json({mssg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
