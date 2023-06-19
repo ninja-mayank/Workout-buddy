35d3cc3ef4ca88d5bf271363000b2c7b2bfd5045
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-20 00:04:38 +0530

    adding react context

--- frontend/src/components/workoutForm.js
+++ frontend/src/components/workoutForm.js
@@ -1,62 +1,70 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 const WorkoutForm = () => {
-    
-    const[title,setTitle] = useState('');
-    const[load,setLoad] = useState('');
-    const[reps,setReps] = useState('');
-    const[error,setError] = useState(null);
+  const { dispatch } = useWorkoutsContext()
 
-    const handleSubmit = async(e) => {
-        e.preventDefault()
-        const workout = {title,load,reps}
-        const response = await fetch('/api/workouts',{
-            method : 'POST',
-            body : JSON.stringify(workout),
-            headers : {
-                'Content-Type' : 'application/json'
-            }
-        });
-        const json = await response.json();
-        if(!response.ok){
-            setError(json.error);
-        }
-        if(response.ok){
-            setError(null)
-            setTitle('')
-            setLoad('')
-            setReps('')
-            console.log('new workout added',json)
-        }
+  const [title, setTitle] = useState('')
+  const [load, setLoad] = useState('')
+  const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const workout = {title, load, reps}
+    
+    const response = await fetch('/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const json = await response.json()
+
+    if (!response.ok) {
+      setError(json.error)
+    }
+    if (response.ok) {
+      setError(null)
+      setTitle('')
+      setLoad('')
+      setReps('')
+      dispatch({type: 'CREATE_WORKOUT', payload: json})
     }
 
-    return ( 
-        <form className="create" onSubmit={handleSubmit}>
-            <h3>Add a New Workout</h3>
-            <label>Exercise title:
-                <input
-                    type="text"
-                    onChange={(e) => setTitle(e.target.value)}
-                    value = {title}
-                />
-            </label>
-            <label>Loads(in Kg):
-                <input
-                    type="number"
-                    onChange={(e) => setLoad(e.target.value)}
-                    value = {load}
-                />
-            </label>
-            <label>Rep:
-                <input
-                    type="text"
-                    onChange={(e) => setReps(e.target.value)}
-                    value = {reps}
-                />
-            </label>
-            <button>Add workout</button>
-        </form>
-    );
+  }
+
+  return (
+    <form className="create" onSubmit={handleSubmit}> 
+      <h3>Add a New Workout</h3>
+
+      <label>Excersize Title:</label>
+      <input 
+        type="text" 
+        onChange={(e) => setTitle(e.target.value)} 
+        value={title}
+      />
+
+      <label>Load (in kg):</label>
+      <input 
+        type="number" 
+        onChange={(e) => setLoad(e.target.value)} 
+        value={load}
+      />
+
+      <label>Number of Reps:</label>
+      <input 
+        type="number" 
+        onChange={(e) => setReps(e.target.value)} 
+        value={reps} 
+      />
+
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  )
 }
- 
-export default WorkoutForm;
+
+export default WorkoutForm
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+// components
 import WorkoutDetails from "../components/workoutDetails"
 import WorkoutForm from "../components/workoutForm"
 
 const Home = () => {
+  const { workouts, dispatch } = useWorkoutsContext()
 
-    const [workouts,setWorkouts] = useState(null)
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
 
-    useEffect(() => {
-        const fetchWorkouts = async() => {
-            const response = await fetch('/api/workouts')
-            const json = await response.json();
-            if(response.ok){
-                setWorkouts(json)
-            }
-        }
-        fetchWorkouts();
-    },[])
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
 
-    return (
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout) => (
-                    <WorkoutDetails key={workout._id} workout = {workout}/>
-                ))}
-            </div>
-            <WorkoutForm />
-        </div>
-    )
+    fetchWorkouts()
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  )
 }
 
 export default Home
