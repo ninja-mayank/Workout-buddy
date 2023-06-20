e9842e20abbd9c73f306d107600cde03214c3ad6
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-20 19:18:26 +0530

    handling fetch errors

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -23,12 +23,28 @@ const getSingleWorkout = async(req,res) => {
 //create a new workout
 const createWorkout = async(req,res) => {
     const {title, load, reps} = req.body
-  
+
+    let emptyFields = []
+
+    if (!title) {
+        emptyFields.push('title')
+    }
+    if (!load) {
+        emptyFields.push('load')
+    }
+    if (!reps) {
+        emptyFields.push('reps')
+    }
+    if (emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
+    }
+
+    // add to the database
     try {
-        const workout = await Workout.create({title, load, reps})
+        const workout = await Workout.create({ title, load, reps })
         res.status(200).json(workout)
     } catch (error) {
-        res.status(400).json({error: error.message})
+        res.status(400).json({ error: error.message })
     }
 }
 
