d4b076f217e4e9c24a23415483f68f28fa2fc1f9
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-22 17:40:16 +0530

    signing tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,19 +1,27 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    jwt.sign({_id : _id},process.env.SECRET,{expiresIn : '3d'})
+}
 
 // login a user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'login user'})
+  res.json({mssg: 'login user'})
 }
 
 // signup a user
 const signupUser = async (req, res) => {
-    const {email,password} = req.body
-    try {
-    const user = await User.signup(email,password)
-    res.status(200).json({email,user});
-    } catch (error) {
-    res.status(400).json({error : error.message})
-    }
+  const {email, password} = req.body
+
+  try {
+    const user = await User.signup(email, password)
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
 module.exports = { signupUser, loginUser }
