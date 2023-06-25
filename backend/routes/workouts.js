36efd935f20a582f43e97a3e3eb61f7eb57cf8be
ninja-mayank <112952488+ninja-mayank@users.noreply.github.com>
2023-06-25 14:30:01 +0530

    protected api routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -6,7 +6,10 @@ const{
     deleteWorkout,
     updateWorkout
 } = require('../controllers/workoutControllers')
+const requireAuth = require('../middleware/requireAuth')
 const router = express.Router()
+
+router.use(requireAuth)
 
 // GET all workouts
 router.get('/', getWorkout)
